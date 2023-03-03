ee8ec206650739c7deff0b97176f75aab821afee
Casey Woodman <caseytwoodman@gmail.com>
2023-03-02 22:00:03 -0600

    added User and reaction models

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    userName: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, "Please enter a valid e-mail address"] },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual for friend count
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
